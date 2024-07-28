c07f37cdcf5ec5503184ec4a0c3f6fb41848a6d4
vnisavic <vnisavic1998@gmail.com>
2024-07-28 13:16:41 +0200

    added reset passwrod page tests

--- PageObjectModels/resetPassPage.js
+++ PageObjectModels/resetPassPage.js
@@ -6,15 +6,19 @@ class ResetPassPage{
         this.emailInput = page.locator("input[placeholder='Enter your email address']")
         this.newPassInput = page.locator('#userPassword')
         this.confirmNewPassInput = page.locator('#confirmPassword')
-        this.saveNewPassBtn = page.locator('#confirmPassword')
+        this.saveNewPassBtn = page.locator("button[type='submit']")
         this.loginLink = page.locator("body > app-root:nth-child(1) > app-password-new:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > form:nth-child(1) > div:nth-child(5) > a:nth-child(1)")
         this.registerLink = page.locator("body > app-root:nth-child(1) > app-password-new:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > form:nth-child(1) > div:nth-child(5) > a:nth-child(2)")
 
     }
 
-    async resetPassword(email, pass, confirmPass){
+    async resetPassword(email, passForReset){
 
-        
+        await this.emailInput.fill(email)
+        await this.newPassInput.fill(passForReset)
+        await this.confirmNewPassInput.fill(passForReset)
+        await this.saveNewPassBtn.click()
+
     }
 
 
--- tests/resetPasswordPageFuncTest.spec.js
+++ tests/resetPasswordPageFuncTest.spec.js
@@ -19,8 +19,36 @@ test.describe('Testing reset password functionality', ()=>{
     test('Check if password reset actually resets the password', async({page})=>{
 
         const poManager = new PageObjectManager(page)
+        const loginPage = poManager.getLoginPage()
         const resetPassPage = poManager.getResetPassPage()
 
-        
+        await resetPassPage.resetPassword(credentials.validEmail, credentials.passwordForReset) //set a new password, change reset pass value in credentials.json to test again
+
+        //try to log in with new password
+        await loginPage.logIn(credentials.validEmail, credentials.passwordForReset)
+        await page.waitForTimeout(1000)
+        await expect(page.url()).toBe(urls.homePageUrl)
+
     })
+
+    test('Check if login page link on reset password page redirects to the correct page', async({page})=>{
+
+        const poManager = new PageObjectManager(page)
+        const resetPassPage = poManager.getResetPassPage()
+
+        await resetPassPage.loginLink.click()
+        await expect(page.url()).toBe(urls.loginPageUrl)
+    
+    })
+
+    test('Check if register page link on reset password page redirects to the correct page', async({page})=>{
+
+        const poManager = new PageObjectManager(page)
+        const resetPassPage = poManager.getResetPassPage()
+
+        await resetPassPage.registerLink.click()
+        await expect(page.url()).toBe(urls.registerPageUrl)
+    
+    })
+
 })
