63a0407ed5f5b3bac0b6c911d652edb6aee2aab7
vnisavic <vnisavic1998@gmail.com>
2024-08-12 13:07:42 +0200

    added some register page tests

--- PageObjectModels/registerPagePom.js
+++ PageObjectModels/registerPagePom.js
@@ -8,7 +8,7 @@ class RegisterPage {
         this.emailInput = page.locator('#userEmail')
         this.phoneInput = page.locator('#userMobile')
         this.ocupationDropdown = page.locator('select')
-        this.ocupationStudent = page.locator("option[value*='2']")
+        this.ocupationStudent = this.ocupationDropdown.locator("option[value*='2']")
         this.genderMaleCheckBox = page.locator("input[value='Male']")
         this.genderFemaleCheckBox = page.locator("input[value='Female']")
         this.passwordInput = page.locator('#userPassword')
@@ -17,12 +17,14 @@ class RegisterPage {
         this.registerBtn = page.locator('#login')
         this.loginHereLink = page.locator('.login-wrapper-footer-text')
         this.requiredInputMessages = page.locator('.ng-star-inserted')
+        this.regPageLoginBtn = page.locator('.btn.btn-primary')
 
     }
 
     async goToRegisterPage(){
 
-        await this.page.goto('https://rahulshettyacademy.com/client/auth/register')
+        await this.page.goto('https://rahulshettyacademy.com/client')
+        await this.page.locator('.login-wrapper-footer-text').click()
 
     }
 
@@ -35,11 +37,52 @@ class RegisterPage {
         await this.passwordInput.fill(pass)
         await this.confirmPassInput.fill(pass)
         await this.ocupationDropdown.click()
-        await this.ocupationStudent.click()
+
+        //clicking a hidden option
+        let dropdownOptions = await this.page.$$(".ng-star-inserted")
+        for(let option of dropdownOptions){
+
+            let optionText = await option.textContent()
+            option.style.display = 'block'
+
+            if(optionText.includes("Student")){
+
+                await option.click()
+
+            }
+        }
+
         await this.ageCheckbox.click()
         await this.registerBtn.click()
-        
+
+        await this.regPageLoginBtn.click()
+
     }
+
+    async checkEmptyInputWarnigns(){
+
+        let areWarningsVisible = false
+
+        await this.registerBtn.click()
+
+        let warnignMessages = await this.page.$$("//div[contains(text(),'required')]")
+        let checkThisCheckbox = this.page.locator("//div[contains(text(),'*Please check above checkbox')]")
+        let checkBoxWarnignText = await checkThisCheckbox.textContent()
+
+        for(let warning of warnignMessages){
+
+            let warningText = await warning.textContent()
+            if(await warningText.includes('required') && await checkBoxWarnignText.includes('Please check')){
+
+                areWarningsVisible = true
+            }
+
+        }
+
+        return areWarningsVisible
+    }
+
+    
 }
 
 module.exports = {RegisterPage}
--- tests/registerPageFuncTest.spec.js
+++ tests/registerPageFuncTest.spec.js
@@ -1,5 +1,7 @@
 const {test, expect} = require ('@playwright/test')
 const {PageObjectManager} = require ('../PageObjectModels/pageObjectManager')
+import registerInfo from "../jsonFiles/registerCredentials.json"
+import url from "../jsonFiles/urls.json"
 
 test.describe('Testing register page functionality', ()=>{
 
@@ -11,11 +13,27 @@ test.describe('Testing register page functionality', ()=>{
 
     })
 
-    test('Register a new account and try to login with it', async({page})=>{
+    test.skip('Register a new account and try to login with it', async({page})=>{//before running this test again change the register credentials to avoid error
 
         const poManager = new PageObjectManager(page)
         const registerPage = poManager.getRegisterPage()
+        const loginPage = poManager.getLoginPage()
 
-        await registerPage.registerNewAccount()
+        await registerPage.registerNewAccount(registerInfo.registerFirstName, registerInfo.registerLastName,registerInfo.registerEmail,registerInfo.registerPassword,registerInfo.registerPhone)
+        await loginPage.logIn(registerInfo.registerEmail, registerInfo.registerPassword)
+        await page.waitForTimeout(1000)
+        await expect(page.url()).toBe(url.homePageUrl)
+
     })
+
+    test("Check if required input boxes show a warning if no data is entered", async({page})=>{
+
+        const poManager = new PageObjectManager(page)
+        const registerPage = poManager.getRegisterPage()
+        let checkWarnings = await registerPage.checkEmptyInputWarnigns()
+        await expect(checkWarnings).toBeTruthy()
+
+    })
+
+    
 })
