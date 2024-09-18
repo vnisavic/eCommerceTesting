6b48a94038185a36bef9a35cdb306f80a3063417
vnisavic <vnisavic1998@gmail.com>
2024-09-18 13:25:46 +0200

    started testing cart page

--- PageObjectModels/cartPagePom.js
+++ PageObjectModels/cartPagePom.js
@@ -1,45 +1,61 @@
 const {expect, request} = require ('@playwright/test')
-const userDataPayload = {userEmail:"nekimail@gmail.com", userPassword:"Karamela1234&"}
-const zaraCoatPayload = {id:"6581ca399fd99c85e8ee7f45"}
-let loginToken 
 
 class CartPage{
 
     constructor(page){
 
         this.page = page
-
+        this.coatDeleteBtn = this.page.locator("ul:nth-child(1) li:nth-child(1) div:nth-child(1) div:nth-child(3) button:nth-child(2)")
+        this.shoesDeleteBtn = this.page.locator("li[class='items odd ng-star-inserted'] button[class='btn btn-danger']")
+        this.phoneDeleteBtn = this.page.locator("body > app-root:nth-child(1) > app-profile:nth-child(2) > div:nth-child(2) > div:nth-child(2) > ul:nth-child(3) > li:nth-child(1) > div:nth-child(1) > div:nth-child(3) > button:nth-child(2) > i:nth-child(1)")
+        this.noItemsText = this.page.locator("div[class='ng-star-inserted'] h1")
     }
 
-    async getLoginToken(){
+    async deleteItemsFromCart(){
 
-        const apiContext = await request.newContext()
-        const loginResponse = await apiContext.post('https://rahulshettyacademy.com/api/ecom/auth/login', {data:userDataPayload})
-        const responseBody = await loginResponse.json()
-        loginToken = responseBody.token //getting login token
+        let allDeleteBtns = [this.coatDeleteBtn, this.phoneDeleteBtn, this.shoesDeleteBtn]
+        for(let deleteBtn of allDeleteBtns){
+
+            await deleteBtn.click()
+
+        }
+        await this.page.waitForTimeout(1000)
+        let noItemsInCart = await this.noItemsText.textContent()
+        console.log(noItemsInCart)
+        await expect(noItemsInCart).toContain("No Products")
+    }
+
+    // async getLoginToken(){
+
+    //     const apiContext = await request.newContext()
+    //     const loginResponse = await apiContext.post('https://rahulshettyacademy.com/api/ecom/auth/login', {data:userDataPayload})
+    //     const responseBody = await loginResponse.json()
+    //     loginToken = responseBody.token //getting login token
      
-    }
+    // }
 
-    async addItemToCartApi(){
+    // async addItemToCartApi(){
 
-        console.log(loginToken)
+    //     console.log(loginToken)
         
-        const apiContext = await request.newContext()
+    //     const apiContext = await request.newContext()
 
-        const addToCartRequest = await apiContext.post('https://rahulshettyacademy.com/api/ecom/user/add-to-cart',
-            {data:zaraCoatPayload},
+    //     const addToCartRequest = await apiContext.post('https://rahulshettyacademy.com/api/ecom/user/add-to-cart',
+    //         {data:zaraCoatPayload},
 
-            {headers:{
+    //         {headers:{
 
-                'Authorization' : loginToken,
-                'Content-type' : 'application/json'
+    //             'Authorization' : loginToken,
+    //             'Content-type' : 'application/json'
 
-            }}
-        )
+    //         }}
+    //     )
 
-        const response = await addToCartRequest.json()
-        console.log(response)
+    //     const response = await addToCartRequest.json()
+    //     console.log(response)
         
-    }
+    // }
+
+
 }
 module.exports = {CartPage}
---
+++ tests/cartPageTest.spec.js
@@ -0,0 +1,26 @@
+const {test, expect} = require ('@playwright/test')
+const {PageObjectManager} = require ('../PageObjectModels/pageObjectManager')
+const { CartPage } = require('../PageObjectModels/cartPagePom')
+
+test.describe('Verifying that all cart page functionalities are working', ()=>{
+
+    test.beforeEach('Log in', async({page})=>{
+
+        const poManager = new PageObjectManager(page)
+        const homePage = await poManager.getHomePage()
+        await homePage.loginWithApi()
+
+    })
+
+    test('Verify that the delete button is deleting an item from cart', async({page})=>{
+
+        const poManager = new PageObjectManager(page)
+        const homePage = await poManager.getHomePage()
+        const cartPage = await poManager.getCartPage()
+
+        await homePage.checkAddToCartBtns()
+        await homePage.myCartBtn.click()
+        await cartPage.deleteItemsFromCart()
+        
+    })
+})
