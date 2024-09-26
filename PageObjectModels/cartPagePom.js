733fbfd0d08adc62ca35c3173a41063f5f80f547
vnisavic <vnisavic1998@gmail.com>
2024-09-26 11:41:18 +0200

    deleted some code

--- PageObjectModels/cartPagePom.js
+++ PageObjectModels/cartPagePom.js
@@ -19,45 +19,13 @@ class CartPage{
             await deleteBtn.click()
 
         }
-        
+
         await this.page.waitForTimeout(1000)
         let noItemsInCart = await this.noItemsText.textContent()
         console.log(noItemsInCart)
         await expect(noItemsInCart).toContain("No Products")
 
     }
-
-    // async getLoginToken(){
-
-    //     const apiContext = await request.newContext()
-    //     const loginResponse = await apiContext.post('https://rahulshettyacademy.com/api/ecom/auth/login', {data:userDataPayload})
-    //     const responseBody = await loginResponse.json()
-    //     loginToken = responseBody.token //getting login token
-     
-    // }
-
-    // async addItemToCartApi(){
-
-    //     console.log(loginToken)
-        
-    //     const apiContext = await request.newContext()
-
-    //     const addToCartRequest = await apiContext.post('https://rahulshettyacademy.com/api/ecom/user/add-to-cart',
-    //         {data:zaraCoatPayload},
-
-    //         {headers:{
-
-    //             'Authorization' : loginToken,
-    //             'Content-type' : 'application/json'
-
-    //         }}
-    //     )
-
-    //     const response = await addToCartRequest.json()
-    //     console.log(response)
-        
-    // }
-
 
 }
 module.exports = {CartPage}
